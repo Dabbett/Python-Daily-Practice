f66676f83bdd7a52b7bd1d3ec01521a102b6098a
Dylan Abbett <dylanabbett@Dylans-MacBook-Pro.local>
2024-03-25 14:03:49 -0400

    adding functionality to the prompt and response

--- hello.py
+++ hello.py
@@ -3,8 +3,18 @@
 
 # name = input("what is your name?").strip().title()
 
-# print("Hello," name)
+# print(f"Hello, {name}")
 
 
-objects =["hello", "world", "I", "enjoy", "full", "stack", "code"]
-print(*objects, sep=' ', end=".\n")
+# objects =["hello", "world", "I", "enjoy", "full", "stack", "code"]
+# print(*objects, sep=' ', end=".\n")
+
+# another way:
+
+name = input("what is your first and last name?").strip().title()
+
+first, last = name.split(" ")
+
+print(f"Hello, {first}")
+
+print(f"Hello Mr/Mrs {last}, may I call you {first}?")
